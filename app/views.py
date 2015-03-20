efada5cfe23064d3df06587b61c2bb70463dd00a
stevesark <krasnovetskiy@gmail.com>
2015-03-20 08:58:15 -0400

    Modify run.py to remote access

--- app/views.py
+++ app/views.py
@@ -1,34 +1,33 @@
+from flask import render_template, flash, redirect
+from app import app
+from forms import LoginForm
 
-
-
-    	posts = posts)
-    	title = 'Home',
-    	user = user,
-            'author': { 'nickname': 'John' },
-            'author': { 'nickname': 'Susan' },
-            'body': 'Beautiful day in Portland!'
-            'body': 'The Avengers movie was so cool!'
-        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-        form = form,
-        providers = app.config['OPENID_PROVIDERS'])
-        return redirect('/index')
-        title = 'Sign In',
-        {
-        {
-        }
-        },
-    ]
-    form = LoginForm()
-    if form.validate_on_submit():
-    posts = [
-    return render_template("index.html",
-    return render_template('login.html',
-    user = { 'nickname': 'Miguel' }
 @app.route('/')
 @app.route('/index')
-@app.route('/login', methods = ['GET', 'POST'])
 def index():
+    user = { 'nickname': 'Miguel' }
+    posts = [
+        { 
+            'author': { 'nickname': 'John' }, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': { 'nickname': 'Susan' }, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+    return render_template('index.html',
+        title = 'Home',
+        user = user,
+        posts = posts)
+
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
-from app import app
-from flask import render_template, flash, redirect
-from forms import LoginForm
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', 
+        title = 'Sign In',
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
