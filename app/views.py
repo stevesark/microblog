acd35ded7e4489a752c95087af56f7e65ff8fc03
stevesark <krasnovetskiy@gmail.com>
2015-03-31 12:19:37 +0300

    Email support

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from forms import LoginForm, EditForm, PostForm, SearchForm
 from models import User, ROLE_USER, ROLE_ADMIN, Post
 from datetime import datetime
 from config import POSTS_PER_PAGE, MAX_SEARCH_RESULTS
+from emails import follower_notification
 
 @lm.user_loader
 def load_user(id):
@@ -136,6 +137,7 @@ def follow(nickname):
     db.session.add(u)
     db.session.commit()
     flash('You are now following ' + nickname + '!')
+    # follower_notification(user, g.user)
     return redirect(url_for('user', nickname = nickname))
 
 @app.route('/unfollow/<nickname>')
--- config.py
+++ config.py
@@ -17,10 +17,12 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 # mail server settings
-MAIL_SERVER = 'localhost'
-MAIL_PORT = 25
-MAIL_USERNAME = None
-MAIL_PASSWORD = None
+MAIL_SERVER = 'smtp.googlemail.com' #'localhost'
+MAIL_PORT = 465 #25
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = '' #None
+MAIL_PASSWORD = ''# None
 
 # administrator list
 ADMINS = ['krasnovetskiy@yahoo.com']
