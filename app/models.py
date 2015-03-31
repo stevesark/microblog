e6e166827cb1a71cc82cb22580241370388be127
stevesark <krasnovetskiy@gmail.com>
2015-03-31 10:51:13 +0300

    Полнотестовый поиск

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from app import db
 from hashlib import md5
+from app import app
+import flask.ext.whooshalchemy as whooshalchemy
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
@@ -72,6 +74,8 @@ class User(db.Model):
 
 
 class Post(db.Model):
+    __searchable__ = ['body']
+
     id = db.Column(db.Integer, primary_key = True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime)
@@ -80,3 +84,4 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post %r>' % (self.body)
 
+whooshalchemy.whoosh_index(app, Post)
--- config.py
+++ config.py
@@ -27,3 +27,7 @@ ADMINS = ['krasnovetskiy@yahoo.com']
 
 # pagination
 POSTS_PER_PAGE = 3
+
+WHOOSH_BASE = os.path.join(basedir, 'search.db')
+
+MAX_SEARCH_RESULTS = 50
